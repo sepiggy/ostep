552ebe144070081ee79d41aa9ad4760d11214b7d
贾明盛 <jms1209@qq.com>
2018-02-28 14:20:08 +0800

    ch02 ok

--- code/ch02.intro/io.c
+++ code/ch02.intro/io.c
@@ -5,21 +5,11 @@
 #include <sys/types.h>
 #include <string.h>
 
-void do_work() {
-    int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, 
-		  S_IRUSR | S_IWUSR);
-    assert(fd >= 0);
-    char buffer[20];
-    sprintf(buffer, "hello world\n");
-    int rc = write(fd, buffer, strlen(buffer));
-    assert(rc == strlen(buffer));
-    printf("wrote %d bytes\n", rc);
-    fsync(fd);
-    close(fd);
+int main() {
+  int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+  assert(fd > -1);
+  int rc = write(fd, "hello world\n", 13);
+  assert(rc == 13);
+  close(fd);
+  return 0;
 }
-
-int main(int argc, char *argv[]) {
-    do_work();
-    return 0;
-}
-
--- code/ch02.intro/mem.c
+++ code/ch02.intro/mem.c
@@ -5,26 +5,17 @@
 #include "common.h"
 
 int
-main(int argc, char *argv[])
+main()
 {
-    if (argc != 2) { 
-	fprintf(stderr, "usage: mem <value>\n"); 
-	exit(1); 
-    } 
-    int *p;                   // memory for pointer is on "stack"
-    p = malloc(sizeof(int));  // malloc'd memory is on "heap"
-    assert(p != NULL);
-    printf("(pid:%d) addr of p:        %llx\n", (int) getpid(), 
-	   (unsigned long long) &p);
-    printf("(pid:%d) addr stored in p: %llx\n", (int) getpid(), 
-	   (unsigned long long) p);
-    *p = atoi(argv[1]);       // assign value to addr stored in p
-    while (1) {
-	Spin(1);
-	*p = *p + 1;
-	printf("(pid:%d) value of p: %d\n", getpid(), *p);
-    }
-
-    return 0;
+  int *p = malloc(sizeof(int));
+  assert(p != NULL);
+  printf("(%d) address pointed to by p: %p\n", getpid(), p);
+  *p = 0;
+  while (1) {
+    Spin(1);
+    *p = *p + 1;
+    printf("(%d) p: %d\n", getpid(), *p);
+  }
+  return 0;
 }
 
